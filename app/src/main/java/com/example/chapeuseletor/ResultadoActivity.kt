637d2c4a48a7cd3f53c84de68844a5bc979f8818
Dougllascampos <douglascamposdutra@gmail.com>
2025-09-02 21:17:01 -0300

    ChapeuSeletor

--- app/src/main/java/com/example/chapeuseletor/ResultadoActivity.kt
+++ app/src/main/java/com/example/chapeuseletor/ResultadoActivity.kt
@@ -1,11 +1,13 @@
 package com.example.chapeuseletor
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
